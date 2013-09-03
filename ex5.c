d0aeea3a19c091f57a7724f9026d9ce8f4cbd977
Your NameRuben <rgomez@wl00030800.active.local>
2013-09-03 13:11:00 -0700

    Longest word

--- ex5.c
+++ ex5.c
@@ -2,7 +2,7 @@
 int main(int argc, char *argv[])
 {
 	int areas[] = {10,2,13,14,20};
-	char name[] = "Nates";
+	char name[] = "pneumonoultramicroscopicsilicovolcanoconiosis";
 	char full_name[] = {'N', 'a', 't', 'e', 'D', 'o', 'g', 'g', '\0'};
 
 	printf("My name is %s\n", name);
@@ -11,5 +11,6 @@ int main(int argc, char *argv[])
 	for ( int x = 0; x < sizeof(name)/sizeof(char)-1; x++ ){
 		printf("%c\n", name[x]);
 	}
+	
 	return 0;
 }
